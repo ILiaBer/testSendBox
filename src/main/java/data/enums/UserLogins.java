d65f11b0f747f6187c96ab317b6e9b51a60a50e8
Berestov <berestov@protei.ru>
2023-11-07 22:26:33 +0300

    07.11

--- src/main/java/data/enums/UserLogins.java
+++ src/main/java/data/enums/UserLogins.java
@@ -9,8 +9,8 @@ public enum UserLogins {
 
     STANDARD_USER("standard_user"),
     LOCKED_OUT_USER("locked_out_user"),
-    PROBLEM_USER("problem_user"),
-    PERFORMANCE_GLITCH_USER("performance_glitch_user");
+    PROBLEM_USER("problem_user12"),
+    PERFORMANCE_GLITCH_USER("performance_glitch_user12");
 
     @Getter
     private String name;
